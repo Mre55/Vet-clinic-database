1a83f5bd2d30d272ed7e95ced83007034f9a9f56
mre55 <misganawmihreteab>
2022-02-21 20:42:09 +0300

    Add new column to owners table and insert values to visits and owners table

--- data.sql
+++ data.sql
@@ -76,3 +76,6 @@ INSERT INTO VISITS VALUES (7, 2, '2020-08-03');
 INSERT INTO VISITS VALUES (9, 3, '2020-05-24');
 INSERT INTO VISITS VALUES (9, 1, '2021-01-11');
 
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
